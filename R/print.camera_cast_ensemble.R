8e299cd281fd7f1312230ad8b9b9679fc7aee4de
Janek <janek.thomas@web.de>
2015-04-19 16:38:00 +0200

    added print method for ensemble object

---
+++ R/print.camera_cast_ensemble.R
@@ -0,0 +1,7 @@
+print.camera_cast_ensemble <- function(object){
+  number_optitrack <- sum(object$sensorIds == "OPTITRACK")
+  number_kinect <- length(object$sensorIds) - number_optitrack
+  
+  print(paste(object$n_observations , "observations by", number_kinect, 
+        "Kinect(s) and", number_optitrack, "Optitrack System"))
+}
