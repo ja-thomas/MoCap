18e333a32c22b2ed0ca32178e8ce83da3588f245
Janek <janek.thomas@web.de>
2015-04-19 16:45:08 +0200

    added doc to print function

--- R/print.camera_cast_ensemble.R
+++ R/print.camera_cast_ensemble.R
@@ -1,3 +1,9 @@
+#'print camera cast ensemble
+#'
+#'
+#'@author Janek
+#'@encoding UTF-8
+#'
 print.camera_cast_ensemble <- function(object){
   number_optitrack <- sum(object$sensorIds == "OPTITRACK")
   number_kinect <- length(object$sensorIds) - number_optitrack
