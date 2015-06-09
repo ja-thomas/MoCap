16c03f2b2824e41b3d956dcb00bb58331c675cd8
ja-thomas <janek.thomas@web.de>
2015-06-09 22:24:26 +0200

    added kinematic model data

---
+++ R/kinematic_model.R
@@ -0,0 +1,11 @@
+#'kinematic_model
+#'
+#'Contains a 25x25 neighbourhood matrix. 1 for connected joints, 0 for not connected
+#'
+#'@docType data
+#'@keywords datasets
+#'@format A matrix with 25 rows and columns
+#'@name kinematic_model
+#'
+#'
+NULL
